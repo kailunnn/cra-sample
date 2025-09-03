ef62b73beee4bbd3fbd6964d947e4d3df548ab74
Kailun Hsu <t23795@gmail.com>
2025-09-03 15:40:00 +0800

    add different envs and ajax

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// 外部套件
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// 空一行，放內部套件
+import logo from './assets/logo.svg';
+import './assets/App.css';
+import Input from './components/Input';
+import './assets/all.scss';
 
 function App() {
+
+  const [text, setText] = useState('');
+
+  const onChangeHandler = (e) => {
+    setText(e.target.value);
+  }
+
+  useEffect(() => {
+    (async() => {
+      console.log("API Path:", process.env.REACT_APP_PATH);
+      const path = process.env.REACT_APP_PATH;
+      const result = await axios.get(path);
+      console.log(result);
+    })()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa voluptate, aliquid dicta eligendi in delectus dolor maiores, pariatur reiciendis voluptas harum iure
         </p>
         <a
           className="App-link"
@@ -17,6 +40,9 @@ function App() {
         >
           Learn React
         </a>
+        <button type="button" className="btn btn-primary">Primary</button>
+        {text}
+        <Input id="SampleText" text="這是一個Input" value={text} onChangeHandler={onChangeHandler}/>
       </header>
     </div>
   );
